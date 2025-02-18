b6fb0ef14fdbe0c6d30d5ca6a11f9ea0b5dfc411
Aditya Narkhede <anarkhede@vt.edu>
2025-02-18 00:53:37 -0500

    Implemented the Driver wrapper class.

---
+++ JegaDriver.cpp
@@ -0,0 +1,69 @@
+#include<JegaDriver.h>
+
+using namespace std;
+using namespace JEGA::Logging;
+using namespace JEGA::FrontEnd;
+using namespace eddy::utilities;
+using namespace JEGA::Utilities;
+using namespace JEGA::Algorithms;
+
+//-----------------------------------------------------------------------------
+
+JegaDriver::JegaDriver(const ProblemConfig &config)
+          : JEGA::FrontEnd::Driver(config)
+{
+  EDDY_FUNC_DEBUGSCOPE
+}
+
+//-----------------------------------------------------------------------------
+
+//! Refactored from JEGA::FrontEnd::Driver::PerformIterations
+DesignOFSortSet
+JegaDriver::PerformIterations(GeneticAlgorithm *ga)
+{
+
+  EDDY_FUNC_DEBUGSCOPE
+
+  try{
+
+    // prepare to time this run.
+    JEGA_LOGGING_IF_ON(clock_t start = clock();)
+
+    // initialize the populations
+    ga->AlgorithmInitialize();
+
+    while(true) {
+
+      // This will call JegaEvaluator class through JEGA.
+      if(!PerformNextIteration(ga)) break;
+
+      // Place Adaptive decision model here.
+
+    }
+
+    // Clean-ups on JEGA side.
+    ga->AlgorithmFinalize();
+
+    JEGA_LOGGING_IF_ON(double elapsed = (double)(clock() - start) / CLOCKS_PER_SEC;)
+
+    JEGALOG_II(ga->GetLogger(), lquiet(), this, ostream_entry(lquiet(), 
+               "JEGA Front End: " + theGA->GetName() + " execution took ") 
+               << elapsed << " seconds.")
+
+    JEGALOG_II_G(lquiet(), this, ostream_entry(lquiet(), "JEGA Front End: "
+                 "Execution took ") << elapsed << " seconds.")
+
+    return DeepDuplicate(theGA->GetCurrentSolution());
+
+  } catch(const exception &e) {
+    JEGALOG_II_G_F(this, text_entry(lfatal(), "JEGA Front End Error: "
+                   "Exception caught at application level reading: \"") 
+                   << e.what() << "\".")
+  }
+
+  return DesignOFSortSet();
+
+}
+
+//-----------------------------------------------------------------------------
+
--- JegaDriver.h
+++ JegaDriver.h
@@ -4,6 +4,46 @@
 #include <../Utilities/include/JEGAConfig.hpp>
 #include <../Utilities/include/Logging.hpp>
 
+// JEGA core includes.
+#include <GeneticAlgorithm.hpp>
+#include <GeneticAlgorithmEvaluator.hpp>
 
+// JEGA front end includes.
+#include <../FrontEnd/Core/include/Driver.hpp>
+#include <../FrontEnd/Core/include/ProblemConfig.hpp>
+#include <../FrontEnd/Core/include/AlgorithmConfig.hpp>
+#include <../FrontEnd/Core/include/EvaluatorCreator.hpp>
+
+// Eddy utility includes.
+#include<utilities/include/EDDY_DebugScope.hpp>
+
+// JEGA utility includes.
+#include <../Utilities/include/DesignGroup.hpp>
+
+// Include the main optimizer class to get Model and other dakota things.
+#include<DakotaOptimizer.hpp>
+
+class JegaDriver : public JEGA::FrontEnd::Driver {
+
+public:
+
+  //! Constructor
+  JegaDriver(const JEGA::FrontEnd::ProblemConfig &config);
+
+  //! Wrappers
+  JEGA::Algorithms::GeneticAlgorithm* 
+  ExtractAllData(const JEGA::FrontEnd::AlgorithmConfig &config) {
+    return JEGA::FrontEnd::Driver::ExtractAllData(config);
+  }
+
+  void DestroyAlgorithm(JEGA::Algorithms::GeneticAlgorithm *ga) {
+    JEGA::FrontEnd::Driver::DestroyAlgorithm(ga);
+  }
+
+  //! Custom implementations of JEGA::FrontEnd::Driver functions
+  JEGA::Utilities::DesignOFSortSet 
+  PerformIterations(JEGA::Algorithms::GeneticAlgorithm* ga);
+
+};
 
 #endif
