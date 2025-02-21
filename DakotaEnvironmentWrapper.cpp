a5c2d8879b6369a19adc0375c7d434536f8f26a3
Aditya Narkhede <anarkhede@vt.edu>
2025-02-20 19:30:27 -0500

    Testing ways to differentiate true and error models.

--- DakotaEnvironmentWrapper.cpp
+++ DakotaEnvironmentWrapper.cpp
@@ -1,6 +1,9 @@
 #include<DakotaEnvironmentWrapper.h>
 #include<AdaptiveJegaOptimizer.h>
 
+#include<DakotaModel.hpp>
+#include<DakotaInterface.hpp>
+
 using namespace std;
 using namespace Dakota;
 
@@ -46,6 +49,7 @@ void DakotaEnvironmentWrapper::SetupAdaptiveOptimizer()
     ProblemDescDB &problem_db = problem_description_db();
 
     // get all the available models
+/*
     ModelList &models = problem_db.model_list();
     for(auto &m : models) {
 
@@ -60,6 +64,39 @@ void DakotaEnvironmentWrapper::SetupAdaptiveOptimizer()
     }
 
     abort_handler(-1);
+*/
+
+    shared_ptr<Model> top_model = 
+      topLevelIterator.iterated_model();
+
+    // ProblemDescDB::set does not allow tinkering of analysis
+    // drivers. So, we simply create a new Model with the same
+    // interface. We handle different kinds of evaluations 
+    // in the driver through STATE variables.
+
+    shared_ptr<Model> err_model =
+      ModelUtils::get_model(problem_db);
+/*
+    // print drivers to verify --- they should be the same.
+    Interface &true_interface = top_model->derived_interface();
+    Interface &esti_interface = err_model->derived_interface();
+
+    for(auto &driver : true_interface.analysis_drivers()) 
+      Cout << "True driver: " << driver << std::endl;
+    
+    for(auto &driver : esti_interface.analysis_drivers())
+      Cout << "Esti driver: " << driver << std::endl;
+
+    abort_handler(-1);
+*/
+
+    // add continuous state set variables w/ permissible values
+    // TRUE, INTERP, ERROR
+
+
+    // assign our optimizer.
+    topLevelIterator.assign_rep(
+      make_shared<AdaptiveJegaOptimizer>(problem_db, top_model, err_model));
 
   }
 
